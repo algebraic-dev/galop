fd01261e9978db8fead1311710bff872fa38cded
Sofia Rodrigues <sofia@algebraic.dev>
2023-09-26 08:14:36 -0300

    feat: add submissions script

---
+++ submissions.js
@@ -0,0 +1,34 @@
+let fs = require("fs")
+
+let files = fs.readdirSync("./submissions");
+
+let jsons = files.map(x => JSON.parse(fs.readFileSync("./submissions/" + x).toString('utf-8')))
+
+let jsons2 = jsons.map(x => {
+    let tag = "❌";
+
+    let participant = x.tag == "Err" ? x.data[1].name : x.data.participant.name;
+
+    let log = x.tag == "Ok" ? x.data.log.join("") : x.data[0];
+
+    if (x.tag == "Ok" && x?.data?.data["compile"]) {
+        tag = "🟧"
+    }
+
+    if (x.tag == "Ok" && x?.data?.data["fibbo"]) {
+        tag = "✅"
+    }
+
+    if (x.tag == "Ok" && x?.data?.data["timeout"]) {
+        tag += " 🕑"
+    }
+
+    log = log.replaceAll("\n", " ")
+
+    return {
+        tag, participant, log
+    }
+})
+
+console.log("| name | status | log |\n| :-: | :-:| :- |")
+console.log(jsons2.map(x => "| " + x.participant + " | " + x.tag + " | " + (x.log.substring(0, 200) + (x.log.length > 100 ? "..." : "")) + " | ").join("\n"))
