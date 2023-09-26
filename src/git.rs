210db041eb8c9ea24ed49c327b05be1dd20f5b61
Sofia Rodrigues <sofia@algebraic.dev>
2023-09-26 09:02:19 -0300

    fix: clone recursive

--- src/git.rs
+++ src/git.rs
@@ -4,7 +4,7 @@ use git2::{Repository, Error};
 
 /// Clones the repository and gives it a name
 pub fn clone_directory(git_url: &str, destination_path: PathBuf) -> Result<(), Error> {
-    match Repository::clone(git_url, destination_path) {
+    match Repository::clone_recurse(git_url, destination_path) {
         Ok(_) => Ok(()),
         Err(e) => Err(e)
     }
